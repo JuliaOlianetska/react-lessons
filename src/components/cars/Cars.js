9ccfa583f404c65474c23291eae95b7a150e2bc2
JuliaJulia <ulana_m@ukr.net>
2021-08-17 18:07:46 +0300

    lesson4 hw

--- src/components/cars/Cars.js
+++ src/components/cars/Cars.js
@@ -2,7 +2,14 @@ import {useEffect, useState} from "react";
 import {getCars} from "../../services/car.service";
 import Car from "../car/Car";
 
-export default function Cars(cars) {
+export default function Cars() {
+
+  let [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    getCars().then (value => setCars([value]))
+  }, [])
+
 
   return (
     <div>
