2309513d3e8f7aa84ee2d9f8c27580f17db879c0
JuliaJulia <ulana_m@ukr.net>
2021-08-17 18:33:57 +0300

    lesson4 cw template

---
+++ src/forms/Form.js
@@ -0,0 +1,12 @@
+export default function Form() {
+    
+    
+  return (
+    <div>
+        <form onSubmit={onFormSubmit}>
+            <input type="text" name={'model'}/>
+        </form>
+
+    </div>
+  );
+}
