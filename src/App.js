4d00f2cebf0e0bd050c02886499b6fbce50dede0
JuliaJulia <ulana_m@ukr.net>
2021-08-22 22:50:08 +0300

    lesson5 cw 1 %

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    withRouter
+} from "react-router-dom";
+
 export default function App() {
   return (
+      <Router>
     <div>
-    App
+      <Link to={'/cars'}>to cars page</Link>
+
+      <Route path={'/cars'} render={<Cars/>}></Route>
 
     </div>
+      </Router>
   );
 }
