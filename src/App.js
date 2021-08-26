860bf89e586e38cc30c053f6d28532cb106e4bf0
JuliaJulia <ulana_m@ukr.net>
2021-08-26 17:51:25 +0300

    lesson7 hw 100%

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import {useReducer, useState} from "react";
 
 const reducer= (state, action) => {
-  if (action.obj === '1' && action.diya === '+') {
-    return {...state, a: state.a++}
+  if (action.obj === '1' && action.diya === '+10') {
+    return {...state, a: state.a+10}
   } else if
-  (action.obj === '1' && action.diya === '-') {
-    return {...state, a: state.a--}
-  } else if
-  (action.obj === '2' && action.diya === '+') {
-    return {...state, b: state.b++}
-  } else if
-  (action.obj === '2' && action.diya === '-') {
-    return {...state, b: state.b--}
+  (action.obj === '1' && action.diya === '-2') {
+    return {...state, a: state.a-2}
   }
 
   return {...state};
@@ -19,18 +13,16 @@ const reducer= (state, action) => {
 
 export default function App() {
 
-  let [state, dispatch] = useReducer(reducer,{a:0, b:0});
-let {a,b} = state;
+  let [state, dispatch] = useReducer(reducer,{a:0});
+let {a} = state;
   console.log(state);
 
   return (
     <div>
 <h1>state 1 - {a}</h1>
-      <button onClick={()=>dispatch({obj:'1', diya: '+'})}>increment</button>
-      <button onClick={()=>dispatch({obj:'1', diya:'-'})}>decrement</button>
-      <h1>state 2 - {b}</h1>
-      <button onClick={()=>dispatch({obj:'2', diya:'+'})}>increment</button>
-      <button onClick={()=>dispatch({obj:'2', diya:'-'})}>decrement</button>
+      <button onClick={()=>dispatch({obj:'1', diya: '+10'})}>increment</button>
+      <button onClick={()=>dispatch({obj:'1', diya:'-2'})}>decrement</button>
+
     </div>
   );
 }
