d2711ed7f82a47643ff22166c18bed4fcb8e39b8
JuliaJulia <ulana_m@ukr.net>
2021-08-26 13:29:31 +0300

    lesson7 hw 50%

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useReducer, useState} from "react";
 
-function App() {
+const reducer= (state, action) => {
+  if (action.obj === '1' && action.diya === '+') {
+    return {...state, a: state.a++}
+  } else if
+  (action.obj === '1' && action.diya === '-') {
+    return {...state, a: state.a--}
+  } else if
+  (action.obj === '2' && action.diya === '+') {
+    return {...state, b: state.b++}
+  } else if
+  (action.obj === '2' && action.diya === '-') {
+    return {...state, b: state.b--}
+  }
+
+  return {...state};
+}
+
+export default function App() {
+
+  let [state, dispatch] = useReducer(reducer,{a:0, b:0});
+let {a,b} = state;
+  console.log(state);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<h1>state 1 - {a}</h1>
+      <button onClick={()=>dispatch({obj:'1', diya: '+'})}>increment</button>
+      <button onClick={()=>dispatch({obj:'1', diya:'-'})}>decrement</button>
+      <h1>state 2 - {b}</h1>
+      <button onClick={()=>dispatch({obj:'2', diya:'+'})}>increment</button>
+      <button onClick={()=>dispatch({obj:'2', diya:'-'})}>decrement</button>
     </div>
   );
 }
-
-export default App;
